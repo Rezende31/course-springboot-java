4b518f46729ba588d93494ef9947827fe76be0ff
Leonardo Rezende <leo.rezende31@gmail.com>
2022-04-12 13:26:41 -0300

    Service layer, component registration

--- src/main/java/com/udemy/course/resouces/UserResource.java
+++ src/main/java/com/udemy/course/resouces/UserResource.java
@@ -1,22 +1,35 @@
 package com.udemy.course.resouces;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.course.entities.User;
+import com.udemy.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@Autowired
+	private UserService service;
 	
+	@RequestMapping(method = RequestMethod.GET)  // ...
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "teste", "teste@gmail.com", "99999999", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
