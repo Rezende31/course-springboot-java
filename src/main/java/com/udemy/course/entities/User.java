a672c1698772ef50432ca800b22df4f6f860fc16
Leonardo Rezende <leo.rezende31@gmail.com>
2022-04-19 08:23:39 -0300

    OrderItem, many-to-many association with extra attributes PART 1

--- src/main/java/com/udemy/course/entities/User.java
+++ src/main/java/com/udemy/course/entities/User.java
@@ -17,7 +17,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
