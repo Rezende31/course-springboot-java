0b6f8a9d7f5489c65ddf3fa87767ccea9733cdc3
Leonardo Rezende <leo.rezende31@gmail.com>
2022-04-29 11:32:23 -0300

    Subtotal & Total methods

--- src/main/java/com/udemy/course/entities/Order.java
+++ src/main/java/com/udemy/course/entities/Order.java
@@ -104,6 +104,14 @@ public class Order implements Serializable{
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	public Set<OrderItem> getItems(){
 		return items;
