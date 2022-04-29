d8d8302bc341cddc663373e8f4ba2e3df0dcbb83
Leonardo Rezende <leo.rezende31@gmail.com>
2022-04-29 11:03:00 -0300

    Payment, one-to-one association

--- src/main/java/com/udemy/course/config/TestConfig.java
+++ src/main/java/com/udemy/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemy.course.entities.Category;
 import com.udemy.course.entities.Order;
 import com.udemy.course.entities.OrderItem;
+import com.udemy.course.entities.Payment;
 import com.udemy.course.entities.Product;
 import com.udemy.course.entities.User;
 import com.udemy.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
